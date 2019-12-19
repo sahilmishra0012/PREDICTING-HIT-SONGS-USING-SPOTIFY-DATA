7dd2b1594ca8f89d8279e7a6e23b9709a35601d0
Sahil Mishra <sahilmishra0012@outlook.com>
2019-12-20 02:17:27 +0530

    Analysis File Updated

--- file_analyse.py
+++ file_analyse.py
@@ -1,7 +1,4 @@
 import h5py
-import tables
-# print(tables.open_file('TRAAADZ128F9348C2E.h5', mode='r'))
-# print("="*50)
 f1 = h5py.File('TRBGQAO128EF35A1AD.h5', 'r')
 a=list(f1['metadata']['songs'])[0][9].decode('UTF-8')
 b=list(f1['metadata']['songs'])[0][14].decode('UTF-8')
@@ -12,3 +9,5 @@ print("Album=>",b)
 print("Song=>",c)
 print("Year of Release=>",d)
 print((a,b,c,d))
+k=(a,b,c,d)
+print(len(k))
