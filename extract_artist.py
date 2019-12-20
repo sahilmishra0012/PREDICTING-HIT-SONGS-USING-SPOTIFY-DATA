95a78d980acee6bb6887a4c0dfd4fed3679870f5
Sahil Mishra <sahilmishra0012@outlook.com>
2019-12-20 09:31:00 +0530

    New Changes

--- extract_artist.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-data = pd.read_csv('subset_unique_artists.txt', sep="<SEP>", header=None,engine='python')
-data.columns = ["A","B","C","D"]
-
-data.to_csv('artist.csv',header=True,index=False)
